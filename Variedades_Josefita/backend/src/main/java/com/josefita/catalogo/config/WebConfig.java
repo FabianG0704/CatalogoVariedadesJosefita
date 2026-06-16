c5081675b55054099cb8ea53b82da61e2f408d65
FabianG <FabianG0704>
2026-06-16 15:54:21 -0400

    Ahora las imagenes serán subidas a Cloudinary

--- Variedades_Josefita/backend/src/main/java/com/josefita/catalogo/config/WebConfig.java
+++ Variedades_Josefita/backend/src/main/java/com/josefita/catalogo/config/WebConfig.java
@@ -1,23 +1,11 @@
 package com.josefita.catalogo.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Value("${app.imagenes.ruta}")
-    private String rutaImagenes;
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Las imágenes quedan accesibles en: http://localhost:8080/imagenes/nombre.jpg
-        registry.addResourceHandler("/imagenes/**")
-                .addResourceLocations("file:" + rutaImagenes + "/");
-    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -27,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
                         "http://127.0.0.1:5500",
                         "http://localhost:5501",
                         "http://127.0.0.1:5501",
-                        "https://variedades-josefita.web.app"  // lo actualizas después del deploy de Firebase
+                        "https://variedades-josefita.web.app"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
